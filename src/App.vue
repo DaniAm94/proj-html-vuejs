e1aec447b4385b6ffe2152e042589f55c2e1c9ce
DaniAm94 <dani.amico94@gmail.com>
2024-02-12 16:32:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+  name: 'Mountain Bike App',
+  components: { FontAwesomeIcon }
+}
+</script>
+
+<template></template>
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+//Importo le singole icone
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faEnvelope, faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faArrowRight, faBars, faAngleRight, faAngleLeft, faAt, faLocationDot, faClock, faEnvelope, faCopyright, faFacebookF, faSquareInstagram, faTwitter, faCheck, faXmark, faPhoneVolume);
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App);
+
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+app.mount('#app')
