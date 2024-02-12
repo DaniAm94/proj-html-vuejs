a2f6f4774694231e5683043d1abf46e17081281b
dieghino26 <brotzadiego@gmail.com>
2024-02-12 19:07:54 +0100

    added footer form content

--- src/components/AppFooter/FooterForm.vue
+++ src/components/AppFooter/FooterForm.vue
@@ -1,5 +1,27 @@
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 export default {
-    name: "FooterForm"
+    name: "Footerform",
+    components: { FontAwesomeIcon }
 }
 </script>
+
+<template>
+    <div class="form">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <button>Subscribe
+            <FontAwesomeIcon icon="arrow-right" />
+        </button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+}
+</style>
