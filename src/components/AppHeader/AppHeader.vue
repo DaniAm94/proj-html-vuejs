f1772c8da2cbd0fc1e70e83e7ccbfae4b8992bd2
lelz99 <emanuelesimonetti99@gmail.com>
2024-02-12 19:11:21 +0100

    filled app header component

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import HeaderNav from './HeaderNav.vue';
+export default {
+    name: 'AppHeader',
+    components: { HeaderNav }
+}
+</script>
+
+<template>
+    <header>
+        <nav class="d-flex justify-content-between px-5 align-items-center h-100">
+            <div>logo</div>
+            <HeaderNav />
+            <div class="d-flex">
+                <p>icon</p>
+                <p>btn</p>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+    height: 80px;
+    border-bottom: 1px solid;
+}
+</style>
---
+++ src/components/AppHeader/HeaderNav.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: 'HeaderNav',
+}
+</script>
+
+<template>
+    <ul class="d-flex mb-0 list-unstyled">
+        <li>link</li>
+        <li>link</li>
+        <li>link</li>
+        <li>link</li>
+        <li>link</li>
+        <li>link</li>
+    </ul>
+</template>
+
+
+<style scoped>
+li {
+    margin: 0 20px;
+}
+</style>
