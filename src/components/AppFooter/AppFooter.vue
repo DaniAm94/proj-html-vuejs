a3af83d6f616b4ded67aee65b7744dd06605b31d
dieghino26 <brotzadiego@gmail.com>
2024-02-12 17:56:55 +0100

    added footer imports

---
+++ src/components/AppFooter/AppFooter.vue
@@ -0,0 +1,19 @@
+<script>
+import FooterNav from './FooterNav.vue';
+import FooterForm from './FooterForm.vue';
+import FooterInfo from './FooterInfo.vue';
+export default {
+    name: "AppFooter",
+    components: {
+        FooterForm,
+        FooterNav,
+        FooterInfo
+    }
+}
+</script>
+
+<template>
+    <FooterInfo />
+    <FooterForm />
+    <FooterNav />
+</template>
