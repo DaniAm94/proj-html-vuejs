a3fbc0d35a7f457db6d26869d41c3b6c31c6582a
DaniAm94 <dani.amico94@gmail.com>
2024-02-13 20:20:58 +0100

    added type professional section's carousel function

--- src/components/AppMain/Sections/TypeProfessionalSection.vue
+++ src/components/AppMain/Sections/TypeProfessionalSection.vue
@@ -6,33 +6,61 @@ export default {
     name: 'TypeProfessionalSection',
     components: { SectionCard, CarouselButton },
     data: () => ({
-        elements: profTypeData
-    })
+        elements: profTypeData,
+        currentCards: [],
+    }),
+    computed: {
+        hiddenCard() {
+            return this.elements.find(element => !this.currentCards.includes(element))
+        }
+    },
+    created() {
+        this.currentCards = this.elements.filter((element, i) => i !== this.elements.length - 1)
+    },
+    methods: {
+        goTo(direction) {
+            if (direction === 'right') {
+                this.currentCards.push(this.hiddenCard);
+                this.currentCards.shift()
+            } else {
+                this.currentCards.unshift(this.hiddenCard);
+                this.currentCards.pop()
+            }
+
+        }
+    }
 }
 </script>
 <template>
-    <section id="type-professional">
+    <section id="type-professional" class="py-5">
         <div class="container py-3">
 
-            <h2 class="text-center py-3">Our Professional Cycling Trainings</h2>
+            <h2 class="text-center">Our Professional Cycling Trainings</h2>
+            <span>Learn from the mountain biking expert.</span>
             <div class="row row-cols-4 flex-nowrap">
-                <div v-for="element in elements" class="col">
-                    <SectionCard :hasBlackBorder="true" :hasImg="true" :img="element.img" :hasOverlay="true"
-                        :top-title="element.title" />
+                <div v-for="card in currentCards" class="col">
+                    <SectionCard :hasBlackBorder="true" :hasImg="true" :img="card.img" :hasOverlay="true"
+                        :top-title="card.title" />
                 </div>
             </div>
-            <div class="text-center mt-3">
-                <CarouselButton class="color me-3 border border-black  " direction="left" />
-                <CarouselButton class="color border border-black" direction="right" />
+            <div class="text-center mt-5">
+                <CarouselButton @click="goTo('left')" class="me-3 border border-black  " :direction="'left'"
+                    :backColor="'bg-white'" />
+                <CarouselButton @click="goTo('right')" class="border border-black" :direction="'right'"
+                    :backColor="'bg-white'" />
             </div>
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
-.row {
-    overflow: hidden;
+h2 {
+    font-weight: bold;
+}
 
-
+span {
+    display: block;
+    text-align: center;
+    margin-bottom: 30px;
 }
 </style>
