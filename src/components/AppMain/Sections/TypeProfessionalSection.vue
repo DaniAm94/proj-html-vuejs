b5bde4f452a41abd616363804c4c0d22e9094e35
lelz99 <emanuelesimonetti99@gmail.com>
2024-02-13 10:35:54 +0100

    add block type professional section

--- src/components/AppMain/Sections/TypeProfessionalSection.vue
+++ src/components/AppMain/Sections/TypeProfessionalSection.vue
@@ -1,11 +1,26 @@
 <script>
+import CarouselButton from '../../CarouselButton.vue'
 import CardsList from './CardsList.vue';
 export default {
     name: 'TypeProfessionalSection',
-    components: { CardsList }
+    components: { CardsList, CarouselButton }
 }
 </script>
 <template>
-    <CardsList />
+    <section class="container py-3" id="type-professional">
+        <h2 class="text-center py-3">Titolo type professional</h2>
+        <div class="row"></div>
+        <CardsList />
+        <div class="text-center mt-3">
+            <CarouselButton class="color me-3" direction="left" />
+            <CarouselButton class="color" direction="right" />
+        </div>
+    </section>
 </template>
-<style></style>
+
+<style>
+.row {
+    height: 400px;
+    border: 1px dashed red;
+}
+</style>
