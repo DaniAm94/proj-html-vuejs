497139705387e9d7f7b0712d359ff0767d34c6ec
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-13 17:31:51 +0100

    add jumbotron content, add font

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script>
+import '@fontsource/poppins';
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/900.css';
+
 import AppHeader from './components/AppHeader/AppHeader.vue';
 import AppMain from './components/AppMain/AppMain.vue';
 import AppFooter from './components/AppFooter/AppFooter.vue';
--- src/components/AppMain/MainJumbotron.vue
+++ src/components/AppMain/MainJumbotron.vue
@@ -8,33 +8,68 @@ export default {
 </script>
 <template>
     <section id="jumbotron">
+        <figure>
+            <img src="../../assets/img/slider-bike-4.jpg" alt="bike-4">
+        </figure>
         <div class="container">
-            <h2>Jumbo title</h2>
-            <span>random text</span>
-            <BaseButton :hasArrow="true" :text="'Learn More'" />
+            <!--PREV BUTTON-->
             <CarouselButton class="carousel-button left" :direction="'left'" :backColor="'bg-black'"
                 :color="'text-white'" />
+
+            <!--CAPTION-->
+            <figcaption>
+                <h2 class="high-title text-white ps-5">Unforgettable Cycling Experience</h2>
+                <span class="text-white ps-5">Learn cycling from the pros.</span>
+            </figcaption>
+            <!--BUTTON-->
+            <BaseButton :hasArrow="true" :text="'Learn More'" class="ms-5 base-button" />
+
+
+            <!--NEXT BUTTON-->
             <CarouselButton class="carousel-button right" :direction="'right'" :backColor="'bg-black'"
                 :color="'text-white'" />
         </div>
     </section>
 </template>
+
+
 <style lang="scss" scoped>
 #jumbotron {
-    position: relative;
 
-    background-color: dodgerblue;
 
     .container {
-        height: 700px;
+        max-height: 800px;
 
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: flex-start;
         row-gap: 10px;
+
+        h2 {
+            font-size: 50px;
+            font-weight: 900;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-100%);
+        }
+
+        span {
+            font-size: 17px;
+            position: absolute;
+            top: 50%;
+            transform: translateY(200%);
+        }
+
+        .base-button {
+            position: absolute;
+            top: 50%;
+            transform: translateY(250%);
+        }
     }
 }
+
+
 
 .carousel-button {
     position: absolute;
