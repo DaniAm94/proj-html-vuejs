7eb34bb1771490bd557112e1c6bc1ca38276c67f
dieghino26 <brotzadiego@gmail.com>
2024-02-13 10:25:16 +0100

    added event section blocks

--- src/components/AppMain/Sections/EventsSection.vue
+++ src/components/AppMain/Sections/EventsSection.vue
@@ -5,7 +5,29 @@ export default {
     components: { CardsList },
 }
 </script>
+
+
 <template>
-    <CardsList />
+    <h2>TITOLO</h2>
+    <div class="container">
+        <CardsList />
+    </div>
 </template>
-<style></style>
+
+
+<style scoped lang="scss">
+.container {
+    height: 700px;
+    border: 2px dashed black;
+    display: flex;
+    background-color: aquamarine;
+}
+
+h2 {
+    padding: 20px 30px;
+    border: 2px dashed black;
+    text-align: center;
+    background-color: aquamarine;
+
+}
+</style>
