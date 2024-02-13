08e134dbbf9baea39d5842b1350bdec91c62553f
DaniAm94 <dani.amico94@gmail.com>
2024-02-13 10:36:30 +0100

    completed jumbotron structure

--- src/components/AppMain/MainJumbotron.vue
+++ src/components/AppMain/MainJumbotron.vue
@@ -1,7 +1,43 @@
 <script>
+import { Carousel } from 'bootstrap';
+
 export default {
     name: 'MainJumbotron',
+    components: { Carousel }
 }
 </script>
-<template></template>
-<style></style>
+<template>
+    <section id="jumbotron">
+        <h2>Jumbo title</h2>
+        <span>random text</span>
+        <BaseButton :hasArrow="'true'" :text="'Learn More'" />
+        <CarouselButton class="carousel-button left" :direction="'left'" :backColor="'bg-black'" :color="'text-white'" />
+        <CarouselButton class="carousel-button right" :direction="'right'" :backColor="'bg-black'" :color="'text-white'" />
+    </section>
+</template>
+<style lang="scss" scoped>
+#jumbotron {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    row-gap: 10px;
+    height: 700px;
+    background-color: dodgerblue;
+}
+
+.carousel-button {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+
+    &.left {
+        left: 20px;
+    }
+
+    &.right {
+        right: 20px;
+    }
+}
+</style>
