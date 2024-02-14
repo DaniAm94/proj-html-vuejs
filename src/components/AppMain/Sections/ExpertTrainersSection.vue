46a0e79ccb6f4a35fbed5e158966b18c51e31bb6
MarcoFonti <theemporiumg@gmail.com>
2024-02-14 15:00:35 +0100

    I add commients

--- src/components/AppMain/Sections/ExpertTrainersSection.vue
+++ src/components/AppMain/Sections/ExpertTrainersSection.vue
@@ -1,6 +1,8 @@
 <script>
+
 import { expertTrainers } from '../../../assets/data/index'
 import SectionCard from './SectionCard.vue';
+
 export default {
     name: 'ExpertTrainersSection',
     data: () => ({
@@ -8,39 +10,55 @@ export default {
     }),
     components: { SectionCard },
 }
+
 </script>
+
 <template>
+
+    <!-- SEZIONE ESPERTI -->
     <section>
+
+        <!-- CONTENITORE -->
         <div class="container">
 
             <h2>Our Expert Trainers</h2>
             <p>Learn to ride a bike from the experts.</p>
+
+            <!-- ROW -->
             <div class="row">
                 <div v-for="card in expertTrainers" class="col-3">
+
+                    <!-- CARD -->
                     <SectionCard :hasImg="true" :hasBlackBottom="true" :name="card.name" :img="card.img" />
+
                 </div>
             </div>
 
         </div>
+
     </section>
+
 </template>
 
 <style lang="scss" scoped>
 
-
+// SEZIONE PROFESSIONISTI 
 section {
     background-image: linear-gradient(#fbfbfb 25%,#ffffff);
 
+// CONTENITORE 
 .container {
     min-height: 300px;
     text-align: center;
     margin-top: 100px;
 
+    // TITOLO
     h2 {
         font-weight: bold;
         padding-top: 3px;
     }
+}
 
 }
-}
+
 </style>
