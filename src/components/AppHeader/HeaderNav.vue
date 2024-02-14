97616ec6f4c93c63b2b2cd26cd5c183a5451ec2e
lelz99 <emanuelesimonetti99@gmail.com>
2024-02-14 11:06:13 +0100

    add other content header (2)

--- src/components/AppHeader/HeaderNav.vue
+++ src/components/AppHeader/HeaderNav.vue
@@ -10,14 +10,3 @@ export default {
 <template>
     <li><a href="#">{{ link }}</a></li>
 </template>
-
-
-<style>
-a {
-    text-decoration: none;
-    color: black;
-    font-weight: 600;
-    font-size: 1.1rem;
-    margin: 0 20px;
-}
-</style>
